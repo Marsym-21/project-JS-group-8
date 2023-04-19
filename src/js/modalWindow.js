06c83efa748c5af57925f0c946917345a26d5de0
Marsym-21 <mini.garnett.21@gmail.com>
2023-04-19 08:03:44 +0200

    1

--- src/js/modalWindow.js
+++ src/js/modalWindow.js
@@ -5,61 +5,59 @@ const logoPath = new URL('../images/icons.svg', import.meta.url);
 const modalContainer = document.querySelector('.modal-container');
 const modalBackdrop = document.querySelector('.modal-backdrop');
 
-const newArray=[];
+const newArray = [];
 let booksArray = [];
 
- export function checkLocalStorage() {
- 
-  const getLocalstorage = localStorage.getItem('id') 
+export function checkLocalStorage() {
+  const getLocalstorage = localStorage.getItem('id');
+  console.log(getLocalstorage);
   parseLokalstorage = JSON.parse(getLocalstorage);
 
   if (!parseLokalstorage) {
-    booksArray = []
+    booksArray = [];
   } else {
-    booksArray = newArray.concat(parseLokalstorage)
-   
+    booksArray = newArray.concat(parseLokalstorage);
   }
 }
 
 function checkMessage() {
-
   if (booksArray.includes(id)) {
-    return message = "Remove from the shopping list"
-  }
-  else {
-   return message ="Add to shopping list"; 
+    return (message = 'Remove from the shopping list');
+  } else {
+    return (message = 'Add to shopping list');
   }
 }
 
 function checkValue(value) {
   if (value !== '') {
-  return value;
-    
-  } else {let message = "No more information about this book"
-  return message;
-}
+    return value;
+  } else {
+    let message = 'No more information about this book';
+    return message;
+  }
 }
 
- export function renderBookInformation() {
+export function renderBookInformation(id) {
+  console.log(id);
   modalBackdrop.classList.remove('is-hidden');
- 
-  modalContainer.innerHTML = "";
-  
+  booksInform = new getBookData(id);
+  modalContainer.innerHTML = '';
+
   booksInform
     .getPromId()
     .then(data => {
+      console.log(data);
+      function links(sms) {
+        const link = data.buy_links;
+        for (let i = 0; i < link.length; i += 1) {
+          if (link[i].name === `${sms}`) {
+            return link[i].url;
+          } else if (link[i].name !== `${sms}`) {
+            continue;
+          }
+        }
+      }
 
-      function links(sms) { 
-        const link = data.buy_links; 
-        for (let i = 0; i < link.length; i += 1) { 
-            if (link[i].name === `${sms}`) { 
-             return link[i].url; 
-            }else if (link[i].name !== `${sms}`) { 
-              continue; 
-          }} 
-      };
-       
-      
       categoryArray = `
        <div class="modal-close__btn-wrapper">
       <button type='button' class='modal-close-btn close'>
@@ -128,68 +126,61 @@ function checkValue(value) {
        </div>
        </div>
        <div  class="modal-book__add__wrapper">  
-       <button type="button" class="modal-book__button" id="${data.id}">${checkMessage()}</button>  
+       <button type="button" class="modal-book__button" id="${
+         data.id
+       }">${checkMessage()}</button>  
    </div>  
         `;
- 
+
       modalContainer.innerHTML = categoryArray;
-        
+
       const closeButton = document.querySelector('.modal-close-btn');
       closeButton.addEventListener('click', closeBtn);
-      
-      const addButton= document.querySelector('.modal-book__button');
+
+      const addButton = document.querySelector('.modal-book__button');
       addButton.addEventListener('click', () => {
-    if (booksArray.includes(id))  {
-    let bookIndex = booksArray.indexOf(id)
-    
-     booksArray.splice(bookIndex, 1)
-     stringKey = JSON.stringify(booksArray); 
-     addButton.textContent = `${checkMessage()}`
-     localStorage.setItem('id', stringKey)
-    return;
-   
-    }  
-      else  {
-    booksArray.push(id);
-    addButton.textContent = `${checkMessage()}`
-    stringKey = JSON.stringify(booksArray); 
-    localStorage.setItem('id', stringKey);
-  }  
-  
-  })  
-  })
+        if (booksArray.includes(id)) {
+          let bookIndex = booksArray.indexOf(id);
+
+          booksArray.splice(bookIndex, 1);
+          stringKey = JSON.stringify(booksArray);
+          addButton.textContent = `${checkMessage()}`;
+          localStorage.setItem('id', stringKey);
+          return;
+        } else {
+          booksArray.push(id);
+          addButton.textContent = `${checkMessage()}`;
+          stringKey = JSON.stringify(booksArray);
+          localStorage.setItem('id', stringKey);
+        }
+      });
+    })
     .catch(error => {
       console.log(error);
     });
 }
 
 function closeBtn() {
-    modalBackdrop.classList.add('is-hidden');
- }
+  modalBackdrop.classList.add('is-hidden');
+}
 
-  
-  export function renderModalWindow(e) {
-    booksInform = new getBookData(id = e.currentTarget.id); 
-    
-  checkLocalStorage();
-  renderBookInformation();
-  }
+export function renderModalWindow(e) {
+  const data = e.currentTarget.id;
+  checkLocalStorage(data);
+  renderBookInformation(data);
+}
 
-
-modalBackdrop.addEventListener('click', (e) => { 
-       
+modalBackdrop.addEventListener('click', e => {
   if (!e.target.classList.contains('modal-backdrop')) {
-      
-  return 
-  }
-  closeBtn(); 
-  
-});
-
-window.addEventListener('keydown', (e) => { 
-  if (e.code != "Escape") {
     return;
   }
-  else {
-    closeBtn()}
-  });
+  closeBtn();
+});
+
+window.addEventListener('keydown', e => {
+  if (e.code != 'Escape') {
+    return;
+  } else {
+    closeBtn();
+  }
+});
