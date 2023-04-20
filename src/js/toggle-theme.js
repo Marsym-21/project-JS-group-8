00a1a1c30328628a49c3ed57b6718857263df3fa
SvitlanaDomanova <majba161@gmail.com>
2023-04-20 12:50:17 +0300

    header change btn theme

--- src/js/toggle-theme.js
+++ src/js/toggle-theme.js
@@ -10,27 +10,23 @@ if (localStorage.getItem('theme') === 'dark') {
 export function getObject() {
   const ref = {
     body: document.querySelector('body'),
+    bookBlockContainer: document.querySelector('.book-block'),
     header: document.querySelector('.header_btn__shopping'),
+    newElem: document.querySelector('.btn-see-more'),
     headerBody: document.querySelector('.header'),
     headerBtnActive: document.querySelector('.current_btn'),
-    spanTitle: document.querySelector('.main-title__first-part'),
-    // headerShopActive: document.querySelector('.current_btn_shopping'),
     headerBtnToogle: document.querySelector('.menu-togle-svg'),
     headerMenuContainer: document.querySelector('.menu-container'),
+    headerMenuMobile: document.querySelector('.mobile-menu'),
     headerUser: document.querySelector('.header_user_name'),
     headerLogoBook: document.querySelector('.header_logo_book'),
     headerBtnMobile: document.querySelector('.header_mobile_shopping'),
+    headerMobileCurrent: document.querySelector('.current_btn_mobile'),
     // headerMobileHome: document.querySelector('.header_mobile_home'),
-    // headerMobileCurrent: document.querySelector('.current_btn_mobile'),
+    headerMobileCurrent: document.querySelector('.current_btn_mobile'),
     headerCloseSvg: document.querySelector('.menu-togle-close'),
-
+    seeMoreBtn: document.querySelector('.btn-see-more'),
     bookBlockContainer: document.querySelector('.book-block'),
-    modaWindow: document.querySelector('.modal-container'),
-    // modalWindowCloseBtn: document.querySelector('.modal-close-btn'),
-    // addButton: document.querySelector('.modal-book__button'),
-    // modalWindowAbout: document.querySelector('.modal-book__about'),
-    // bookSvg: document.querySelector('.book-svg'),
-    // blackBook: document.querySelector('.black-book'),
   };
   const seeMoreBtn = document.querySelectorAll('.btn-see-more');
   console.log(seeMoreBtn);
