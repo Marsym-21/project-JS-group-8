eefb75bad917df6af91e1b1f9c972a915afc1ec8
Marsym-21 <114507956+Marsym-21@users.noreply.github.com>
2023-04-20 11:30:46 +0000

    Deploying to gh-pages from @ Marsym-21/project-JS-group-8@645a8fc0417f618a1e0e97787406d6eff7534f86 🚀

---
+++ shopping-list.90c855a8.js
@@ -0,0 +1,2 @@
+var mybutton=document.getElementById("myBtn");function scrollFunction(){document.body.scrollTop>20||document.documentElement.scrollTop>20?mybutton.style.display="block":mybutton.style.display="none"}function topFunction(){document.body.scrollTop=0,document.documentElement.scrollTop=0}window.onscroll=function(){scrollFunction()};
+//# sourceMappingURL=shopping-list.90c855a8.js.map
